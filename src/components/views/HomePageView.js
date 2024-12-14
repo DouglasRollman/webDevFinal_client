bc8a668f4eedbe6966151031cf3b6d38917a199a
Tangeer Mosobbir <tangeer.mosobbir73@myhunter.cuny.edu>
2024-12-13 19:39:56 -0500

    homepage update

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -7,8 +7,9 @@ It constructs a React component to display the home page.
 const HomePageView = () => {
   // Render Home page view
   return (
-    <div >
+    <div>
       <h1>Home Page</h1>
+      <p>Welcome to Campus Management</p> {/* Added "Welcome to Campus Management" */}
     </div>
   );    
 }
